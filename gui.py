dc4afa8f76c48e8fbcf2c28ccd98aea35adf5382
Luke Tutton <tuttonluke@gmail.com>
2023-06-29 10:31:27 +0100

    Makes buttons bigger.

--- gui.py
+++ gui.py
@@ -8,18 +8,29 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        window_x = 500
+        window_y = 500
+        window_width = 1000
+        window_height = 1000
+
+        button_x = 250
+        button_width = 500
+        button_height = 200
+
+        text_font_size = 15
+        
         self.setWindowTitle("My App")
-        self.setGeometry(500, 500, 300, 200)
+        self.setGeometry(window_x, window_y, window_width, window_height)
         self.setStyleSheet('background-color: black;')
 
-        start_button = QPushButton("start recording", self)
-        start_button.setGeometry(100,25,100,50)
+        start_button = QPushButton("Start Recording", self)
+        start_button.setGeometry(button_x, 125, button_width, button_height)
         start_button.setStyleSheet('background-color: white;')
-        start_button.setFont(QFont('Arial', 10))
-        stop_button = QPushButton("stop recording", self)
-        stop_button.setGeometry(100,125,100,50)
+        start_button.setFont(QFont('Arial', text_font_size))
+        stop_button = QPushButton("Stop Recording", self)
+        stop_button.setGeometry(button_x, 425, button_width, button_height)
         stop_button.setStyleSheet('background-color: white;')
-        stop_button.setFont(QFont('Arial', 10))
+        stop_button.setFont(QFont('Arial', text_font_size))
 
         start_button.clicked.connect(self.start_func)
         stop_button.clicked.connect(self.end_func)
